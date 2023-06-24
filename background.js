c5737185eaa4e4658c3fecb25abb5600b18cfd98
cosisaxis <kishanelewis2@gmail.com>
2023-06-24 04:23:47 -0500

    new fixtures

--- background.js
+++ background.js
@@ -1,37 +1,43 @@
-chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
-  if (changeInfo.status === "complete") {
-    const blocklist = await getBlocklist();
+chrome.alarms.onAlarm.addListener(function (alarm) {
+  if (alarm.name === "studyTimeStart") {
+    chrome.storage.sync.get(["blockedWebsites"], function (result) {
+      const blockedWebsites = result.blockedWebsites || [];
 
-    const url = new URL(tab.url);
-    if (blocklist.includes(url.hostname)) {
-      sendNotification();
-    }
+      chrome.webRequest.onBeforeRequest.addListener(
+        blockRequestListener,
+        { urls: blockedWebsites },
+        ["blocking"]
+      );
+    });
+  } else if (alarm.name === "studyTimeEnd") {
+    chrome.webRequest.onBeforeRequest.removeListener(blockRequestListener);
   }
 });
 
-async function getBlocklist() {
-  return new Promise((resolve, reject) => {
-    chrome.storage.sync.get("blocklist", (result) => {
-      const blocklist = result.blocklist || [];
-      resolve(blocklist);
-    });
+function blockRequestListener(details) {
+  chrome.notifications.create({
+    type: "basic",
+    iconUrl: "icon.png",
+    title: "Blocked Website",
+    message: "You are trying to access a blocked website.",
   });
+
+  sendTextToAccountabilityPartner(details.url);
 }
 
-function sendNotification() {
+function sendTextToAccountabilityPartner(url) {
   const accountSid = "YOUR_TWILIO_ACCOUNT_SID";
   const authToken = "YOUR_TWILIO_AUTH_TOKEN";
-  const client = twilio(accountSid, authToken);
+  const fromNumber = "YOUR_TWILIO_PHONE_NUMBER";
+  const toNumber = "YOUR_ACCOUNTABILITY_PARTNER_PHONE_NUMBER";
 
-  const phoneNumber = "YOUR_FRIENDS_PHONE_NUMBER";
-  const message = "You are wasting time! Focus on your work!";
+  const client = require("twilio")(accountSid, authToken);
 
   client.messages
     .create({
-      body: message,
-      from: "YOUR_TWILIO_PHONE_NUMBER",
-      to: phoneNumber
+      body: `You visited a blocked website: ${url}`,
+      from: fromNumber,
+      to: toNumber,
     })
-    .then(message => console.log("Notification sent!"))
-    .catch(error => console.error(error));
+    .then((message) => console.log(message.sid));
 }
--- popup.js
+++ popup.js
@@ -1,51 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const websiteInput = document.getElementById("websiteInput");
-    const addWebsiteButton = document.getElementById("addWebsiteButton");
-    const blockedWebsitesList = document.getElementById("blockedWebsitesList");
-  
-    addWebsiteButton.addEventListener("click", function () {
-      const website = websiteInput.value.trim();
-      if (website) {
-        chrome.storage.sync.get("blocklist", function (result) {
-          const blocklist = result.blocklist || [];
-          blocklist.push(website);
-          chrome.storage.sync.set({ blocklist }, function () {
-            addBlockedWebsiteToList(website);
-            websiteInput.value = "";
-          });
-        });
+  const websitesInput = document.getElementById("websites");
+  const startTimeInput = document.getElementById("startTime");
+  const endTimeInput = document.getElementById("endTime");
+  const saveButton = document.getElementById("saveBtn");
+
+  saveButton.addEventListener("click", function () {
+    const blockedWebsites = websitesInput.value.split(",").map((website) => website.trim());
+    const startTime = startTimeInput.value;
+    const endTime = endTimeInput.value;
+
+    chrome.storage.sync.set(
+      {
+        blockedWebsites: blockedWebsites,
+        startTime: startTime,
+        endTime: endTime,
+      },
+      function () {
+        alert("Settings saved successfully!");
       }
-    });
-  
-    chrome.storage.sync.get("blocklist", function (result) {
-      const blocklist = result.blocklist || [];
-      blocklist.forEach(function (website) {
-        addBlockedWebsiteToList(website);
-      });
-    });
-  
-    function addBlockedWebsiteToList(website) {
-      const listItem = document.createElement("li");
-      listItem.textContent = website;
-      
-      const removeButton = document.createElement("button");
-      removeButton.textContent = "Remove";
-      removeButton.addEventListener("click", function () {
-        removeBlockedWebsite(website);
-        listItem.remove();
-      });
-      
-      listItem.appendChild(removeButton);
-      blockedWebsitesList.appendChild(listItem);
-    }
-    
-    function removeBlockedWebsite(website) {
-      chrome.storage.sync.get("blocklist", function (result) {
-        const blocklist = result.blocklist || [];
-        const updatedBlocklist = blocklist.filter(item => item !== website);
-        chrome.storage.sync.set({ blocklist: updatedBlocklist });
-      });
-    }
-    
+    );
   });
-  
+});
