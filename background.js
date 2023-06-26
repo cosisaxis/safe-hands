654a07a30f54aad2deb8607738e14098732b060c
cosisaxis <kishanelewis2@gmail.com>
2023-06-26 09:46:48 -0500

    new functionality

--- background.js
+++ background.js
@@ -3,41 +3,78 @@ chrome.alarms.onAlarm.addListener(function (alarm) {
     chrome.storage.sync.get(["blockedWebsites"], function (result) {
       const blockedWebsites = result.blockedWebsites || [];
 
-      chrome.webRequest.onBeforeRequest.addListener(
-        blockRequestListener,
-        { urls: blockedWebsites },
-        ["blocking"]
-      );
+      chrome.action.setBadgeText({ text: "ON" });
+
+      chrome.scripting.executeScript({
+        target: { tabId: -1 },
+        function: blockWebsites,
+        args: [blockedWebsites]
+      });
     });
   } else if (alarm.name === "studyTimeEnd") {
-    chrome.webRequest.onBeforeRequest.removeListener(blockRequestListener);
+    chrome.action.setBadgeText({ text: "" });
+
+    chrome.scripting.executeScript({
+      target: { tabId: -1 },
+      function: unblockWebsites
+    });
   }
 });
 
-function blockRequestListener(details) {
-  chrome.notifications.create({
-    type: "basic",
-    iconUrl: "icon.png",
-    title: "Blocked Website",
-    message: "You are trying to access a blocked website.",
+function blockWebsites(blockedWebsites) {
+  chrome.scripting.insertCSS({
+    target: { cssOrigin: "user" },
+    files: ["blocker.css"]
   });
 
-  sendTextToAccountabilityPartner(details.url);
+  chrome.webNavigation.onBeforeNavigate.addListener(
+    function (details) {
+      const url = new URL(details.url);
+      if (blockedWebsites.includes(url.hostname)) {
+        chrome.scripting.insertCSS({
+          target: { tabId: details.tabId },
+          files: ["blocker.css"]
+        });
+
+        chrome.scripting.executeScript({
+          target: { tabId: details.tabId },
+          function: function () {
+            document.documentElement.innerHTML = "<h1>This website is blocked during your study/work session.</h1>";
+          }
+        });
+
+        // Send text message using Twilio
+        sendTextMessage(url.hostname);
+      }
+    },
+    { url: [{ schemes: ["http", "https"] }] }
+  );
 }
 
-function sendTextToAccountabilityPartner(url) {
+function unblockWebsites() {
+  chrome.webNavigation.onBeforeNavigate.removeListener();
+
+  chrome.scripting.removeCSS({
+    target: { cssOrigin: "user" },
+    files: ["blocker.css"]
+  });
+}
+
+function sendTextMessage(website) {
   const accountSid = "YOUR_TWILIO_ACCOUNT_SID";
   const authToken = "YOUR_TWILIO_AUTH_TOKEN";
-  const fromNumber = "YOUR_TWILIO_PHONE_NUMBER";
-  const toNumber = "YOUR_ACCOUNTABILITY_PARTNER_PHONE_NUMBER";
+  const twilioPhoneNumber = "YOUR_TWILIO_PHONE_NUMBER";
+  const recipientPhoneNumber = "RECIPIENT_PHONE_NUMBER";
 
   const client = require("twilio")(accountSid, authToken);
 
-  client.messages
-    .create({
-      body: `You visited a blocked website: ${url}`,
-      from: fromNumber,
-      to: toNumber,
-    })
-    .then((message) => console.log(message.sid));
+  const messageBody = `Blocked website visited: ${website}`;
+
+  client.messages.create({
+    body: messageBody,
+    from: twilioPhoneNumber,
+    to: recipientPhoneNumber
+  })
+  .then((message) => console.log("Text message sent:", message.sid))
+  .catch((error) => console.error("Error sending text message:", error));
 }
