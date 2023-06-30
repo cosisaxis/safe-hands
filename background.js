d38d88ff3f5ed6b06291c712ee3e850b0138cb55
cosisaxis <kishanelewis2@gmail.com>
2023-06-30 11:15:44 -0500

    background still not working

--- background.js
+++ background.js
@@ -1,59 +1,107 @@
-const accountSid = 'YOUR_TWILIO_ACCOUNT_SID';
-const authToken = 'YOUR_TWILIO_AUTH_TOKEN';
-const twilioPhoneNumber = 'YOUR_TWILIO_PHONE_NUMBER';
-
-const client = require('twilio')(accountSid, authToken);
-
 function sendSMS(phoneNumber) {
-  client.messages
-    .create({
-      body: 'Your friend visited one of the sites they shouldn\'t be visiting.',
-      from: twilioPhoneNumber,
-      to: phoneNumber
-    })
-    .then(message => console.log('SMS sent:', message.sid))
-    .catch(error => console.error('Error sending SMS:', error));
+  const accountSid = "YOUR_TWILIO_ACCOUNT_SID";
+  const authToken = "YOUR_TWILIO_AUTH_TOKEN";
+  const twilioPhoneNumber = "YOUR_TWILIO_PHONE_NUMBER";
+
+  const body =
+    "Your friend visited one of the sites they shouldn't be visiting.";
+  const url = `https://api.twilio.com/2010-04-01/Accounts/${accountSid}/Messages.json`;
+
+  const params = {
+    body,
+    from: twilioPhoneNumber,
+    to: phoneNumber,
+  };
+
+  const headers = {
+    Authorization: "Basic " + btoa(`${accountSid}:${authToken}`),
+    "Content-Type": "application/x-www-form-urlencoded",
+  };
+
+  fetch(url, {
+    method: "POST",
+    headers,
+    body: new URLSearchParams(params),
+  })
+    .then((response) => response.json())
+    .then((data) => console.log("SMS sent:", data.sid))
+    .catch((error) => console.error("Error sending SMS:", error));
 }
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ blockedSites: [] });
 });
 
-chrome.declarativeNetRequest.updateDynamicRules({ removeRuleIds: ['blockRule'] });
+chrome.declarativeNetRequest.updateDynamicRules({
+  removeRuleIds: ["blockRule"],
+});
 
 chrome.storage.onChanged.addListener(({ blockedSites }) => {
   if (blockedSites && blockedSites.newValue) {
     const rule = {
-      id: 'blockRule',
+      id: "blockRule",
       priority: 1,
       action: {
-        type: 'block'
+        type: "block",
       },
       condition: {
         urlFilter: {
-          domains: blockedSites.newValue
-        }
-      }
+          domains: blockedSites.newValue,
+        },
+      },
     };
 
     chrome.declarativeNetRequest.updateDynamicRules({
-      addRules: [rule]
+      addRules: [rule],
     });
   }
 });
 
 chrome.webNavigation.onCommitted.addListener(({ url }) => {
-  chrome.storage.sync.get(['blockedSites'], ({ blockedSites }) => {
+  chrome.storage.sync.get(["blockedSites"], ({ blockedSites }) => {
     if (blockedSites && blockedSites.length > 0) {
-      const visitedBlockedSite = blockedSites.some(site => url.includes(site));
+      const visitedBlockedSite = blockedSites.some((site) =>
+        url.includes(site)
+      );
 
       if (visitedBlockedSite) {
-        chrome.storage.sync.get(['friendPhoneNumber'], ({ friendPhoneNumber }) => {
-          if (friendPhoneNumber) {
-            sendSMS(friendPhoneNumber);
+        chrome.storage.sync.get(
+          ["friendPhoneNumber"],
+          ({ friendPhoneNumber }) => {
+            if (friendPhoneNumber) {
+              sendSMS(friendPhoneNumber);
+            }
           }
-        });
+        );
       }
     }
   });
 });
+
+chrome.storage.sync.get(
+  ["blockedSites", "friendPhoneNumber"],
+  ({ blockedSites, friendPhoneNumber }) => {
+    if (blockedSites) {
+      const rule = {
+        id: "blockRule",
+        priority: 1,
+        action: {
+          type: "block",
+        },
+        condition: {
+          urlFilter: {
+            domains: blockedSites,
+          },
+        },
+      };
+
+      chrome.declarativeNetRequest.updateDynamicRules({
+        addRules: [rule],
+      });
+    }
+
+    if (friendPhoneNumber) {
+      sendSMS(friendPhoneNumber);
+    }
+  }
+);
