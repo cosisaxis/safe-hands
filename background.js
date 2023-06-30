431ef6cc122ddbcb01a4d987e3a12434b91a26c3
cosisaxis <kishanelewis2@gmail.com>
2023-06-30 01:31:14 -0500

    new functionality

--- background.js
+++ background.js
@@ -1,80 +1,59 @@
-chrome.alarms.onAlarm.addListener(function (alarm) {
-  if (alarm.name === "studyTimeStart") {
-    chrome.storage.sync.get(["blockedWebsites"], function (result) {
-      const blockedWebsites = result.blockedWebsites || [];
+const accountSid = 'YOUR_TWILIO_ACCOUNT_SID';
+const authToken = 'YOUR_TWILIO_AUTH_TOKEN';
+const twilioPhoneNumber = 'YOUR_TWILIO_PHONE_NUMBER';
 
-      chrome.action.setBadgeText({ text: "ON" });
+const client = require('twilio')(accountSid, authToken);
 
-      chrome.scripting.executeScript({
-        target: { tabId: -1 },
-        function: blockWebsites,
-        args: [blockedWebsites]
-      });
-    });
-  } else if (alarm.name === "studyTimeEnd") {
-    chrome.action.setBadgeText({ text: "" });
+function sendSMS(phoneNumber) {
+  client.messages
+    .create({
+      body: 'Your friend visited one of the sites they shouldn\'t be visiting.',
+      from: twilioPhoneNumber,
+      to: phoneNumber
+    })
+    .then(message => console.log('SMS sent:', message.sid))
+    .catch(error => console.error('Error sending SMS:', error));
+}
 
-    chrome.scripting.executeScript({
-      target: { tabId: -1 },
-      function: unblockWebsites
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ blockedSites: [] });
+});
+
+chrome.declarativeNetRequest.updateDynamicRules({ removeRuleIds: ['blockRule'] });
+
+chrome.storage.onChanged.addListener(({ blockedSites }) => {
+  if (blockedSites && blockedSites.newValue) {
+    const rule = {
+      id: 'blockRule',
+      priority: 1,
+      action: {
+        type: 'block'
+      },
+      condition: {
+        urlFilter: {
+          domains: blockedSites.newValue
+        }
+      }
+    };
+
+    chrome.declarativeNetRequest.updateDynamicRules({
+      addRules: [rule]
     });
   }
 });
 
-function blockWebsites(blockedWebsites) {
-  chrome.scripting.insertCSS({
-    target: { cssOrigin: "user" },
-    files: ["blocker.css"]
-  });
+chrome.webNavigation.onCommitted.addListener(({ url }) => {
+  chrome.storage.sync.get(['blockedSites'], ({ blockedSites }) => {
+    if (blockedSites && blockedSites.length > 0) {
+      const visitedBlockedSite = blockedSites.some(site => url.includes(site));
 
-  chrome.webNavigation.onBeforeNavigate.addListener(
-    function (details) {
-      const url = new URL(details.url);
-      if (blockedWebsites.includes(url.hostname)) {
-        chrome.scripting.insertCSS({
-          target: { tabId: details.tabId },
-          files: ["blocker.css"]
-        });
-
-        chrome.scripting.executeScript({
-          target: { tabId: details.tabId },
-          function: function () {
-            document.documentElement.innerHTML = "<h1>This website is blocked during your study/work session.</h1>";
+      if (visitedBlockedSite) {
+        chrome.storage.sync.get(['friendPhoneNumber'], ({ friendPhoneNumber }) => {
+          if (friendPhoneNumber) {
+            sendSMS(friendPhoneNumber);
           }
         });
-
-        // Send text message using Twilio
-        sendTextMessage(url.hostname);
       }
-    },
-    { url: [{ schemes: ["http", "https"] }] }
-  );
-}
-
-function unblockWebsites() {
-  chrome.webNavigation.onBeforeNavigate.removeListener();
-
-  chrome.scripting.removeCSS({
-    target: { cssOrigin: "user" },
-    files: ["blocker.css"]
+    }
   });
-}
-
-function sendTextMessage(website) {
-  const accountSid = "YOUR_TWILIO_ACCOUNT_SID";
-  const authToken = "YOUR_TWILIO_AUTH_TOKEN";
-  const twilioPhoneNumber = "YOUR_TWILIO_PHONE_NUMBER";
-  const recipientPhoneNumber = "RECIPIENT_PHONE_NUMBER";
-
-  const client = require("twilio")(accountSid, authToken);
-
-  const messageBody = `Blocked website visited: ${website}`;
-
-  client.messages.create({
-    body: messageBody,
-    from: twilioPhoneNumber,
-    to: recipientPhoneNumber
-  })
-  .then((message) => console.log("Text message sent:", message.sid))
-  .catch((error) => console.error("Error sending text message:", error));
-}
+});
--- popup.js
+++ popup.js
@@ -1,23 +1,54 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const websitesInput = document.getElementById("websites");
-  const startTimeInput = document.getElementById("startTime");
-  const endTimeInput = document.getElementById("endTime");
-  const saveButton = document.getElementById("saveBtn");
-
-  saveButton.addEventListener("click", function () {
-    const blockedWebsites = websitesInput.value.split(",").map((website) => website.trim());
-    const startTime = startTimeInput.value;
-    const endTime = endTimeInput.value;
-
-    chrome.storage.sync.set(
-      {
-        blockedWebsites: blockedWebsites,
-        startTime: startTime,
-        endTime: endTime,
-      },
-      function () {
-        alert("Settings saved successfully!");
-      }
-    );
-  });
+document.addEventListener('DOMContentLoaded', () => {
+  const saveBtn = document.getElementById('saveBtn');
+  saveBtn.addEventListener('click', saveSettings);
+  restoreSettings();
 });
+
+function saveSettings() {
+  const blockedSitesInput = document.getElementById('blockedSites');
+  const friendPhoneNumberInput = document.getElementById('friendPhoneNumber');
+  const startTimeInput = document.getElementById('startTime');
+  const endTimeInput = document.getElementById('endTime');
+
+  const blockedSites = blockedSitesInput.value
+    .split(',')
+    .map(site => site.trim())
+    .filter(site => site.length > 0);
+
+  const friendPhoneNumber = friendPhoneNumberInput.value.trim();
+  const startTime = startTimeInput.value;
+  const endTime = endTimeInput.value;
+
+  chrome.storage.sync.set({
+    blockedSites,
+    friendPhoneNumber,
+    startTime,
+    endTime
+  }, () => {
+    alert('Settings saved!');
+  });
+}
+
+function restoreSettings() {
+  chrome.storage.sync.get(['blockedSites', 'friendPhoneNumber'], ({ blockedSites, friendPhoneNumber }) => {
+    if (blockedSites) {
+      const blockedSitesInput = document.getElementById('blockedSites');
+      blockedSitesInput.value = blockedSites.join(', ');
+    }
+
+    if (friendPhoneNumber) {
+      const friendPhoneNumberInput = document.getElementById('friendPhoneNumber');
+      friendPhoneNumberInput.value = friendPhoneNumber;
+    }
+    if (startTime) {
+      const startTimeInput = document.getElementById('startTime');
+      startTimeInput.value = startTime;
+    }
+
+    if (endTime) {
+      const endTimeInput = document.getElementById('endTime');
+      endTimeInput.value = endTime;
+    }
+  });
+}
+
